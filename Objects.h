fdf7b0880f4e7e41d98a69bb7c396b3bf9e90bba
Claire Hallock <claireehallock@gmail.com>
2023-04-25 12:56:47 -0700

    Added a Cabinet and TV to the Background

--- Objects.h
+++ Objects.h
@@ -43,20 +43,116 @@ public:
         shown = TRUE;
     }
     void paint(){
+        //wall
         glColor3f(1.0f, 0.85f, 0.51f);
         glBegin(GL_QUADS);
-        glVertex3f(-1, -1, depth);
-        glVertex3f(1, -1, depth);
-        glVertex3f(1, 1, depth);
-        glVertex3f(-1, 1, depth);
+            glVertex3f(-1, -1, depth);
+            glVertex3f(1, -1, depth);
+            glVertex3f(1, 1, depth);
+            glVertex3f(-1, 1, depth);
         glEnd();
 
+        //Carpet
         glColor3f(0.96f, 0.4f, 0.28f);
         glBegin(GL_QUADS);
-        glVertex3f(-1, -1, depth);
-        glVertex3f(1, -1, depth);
-        glVertex3f(1, -0.6, depth);
-        glVertex3f(-1, -0.6, depth);
+            glVertex3f(-1, -1, depth);
+            glVertex3f(1, -1, depth);
+            glVertex3f(1, -0.6, depth);
+            glVertex3f(-1, -0.6, depth);
+        glEnd();
+
+        //Main part of Bookshelf
+        glColor3f(0.69f, 0.43f, 0.035f);
+        glBegin(GL_QUADS);
+            glVertex3f(-0.9, 0.8, depth);
+            glVertex3f(-0.2, 0.8, depth);
+            glVertex3f(-0.2, -0.75, depth);
+            glVertex3f(-0.9, -0.75, depth);
+        glEnd();
+
+        //Bookshelf shelves
+        glColor3f(0.53f, 0.33f, 0.027f);
+        glBegin(GL_QUADS);
+            glVertex3f(-0.85, 0.75, depth);
+            glVertex3f(-0.25, 0.75, depth);
+            glVertex3f(-0.25, 0.5, depth);
+            glVertex3f(-0.85, 0.5, depth);
+        glEnd();
+        glBegin(GL_QUADS);
+            glVertex3f(-0.85, 0.45, depth);
+            glVertex3f(-0.25, 0.45, depth);
+            glVertex3f(-0.25, 0.2, depth);
+            glVertex3f(-0.85, 0.2, depth);
+        glEnd();
+        glBegin(GL_QUADS);
+            glVertex3f(-0.85, 0.15, depth);
+            glVertex3f(-0.25, 0.15, depth);
+            glVertex3f(-0.25, -0.1, depth);
+            glVertex3f(-0.85, -0.1, depth);
+        glEnd();
+        glBegin(GL_QUADS);
+            glVertex3f(-0.85, -0.15, depth);
+            glVertex3f(-0.25, -0.15, depth);
+            glVertex3f(-0.25, -0.4, depth);
+            glVertex3f(-0.85, -0.4, depth);
+        glEnd();
+        glBegin(GL_QUADS);
+            glVertex3f(-0.85, -0.45, depth);
+            glVertex3f(-0.25, -0.45, depth);
+            glVertex3f(-0.25, -0.7, depth);
+            glVertex3f(-0.85, -0.7, depth);
+        glEnd();
+
+        //Cabinet for TV
+        glColor3f(0.69f, 0.43f, 0.035f);
+        glBegin(GL_QUADS);
+            glVertex3f(-0.1, -0.2, depth);
+            glVertex3f(0.9, -0.2, depth);
+            glVertex3f(0.9, -0.75, depth);
+            glVertex3f(-0.1, -0.75, depth);
+        glEnd();
+
+        //Cabinet shelves
+        glColor3f(0.62f, 0.38f, 0.05f);
+        glBegin(GL_QUADS);
+            glVertex3f(-0.05, -0.25, depth);
+            glVertex3f(0.85, -0.25, depth);
+            glVertex3f(0.85, -0.45, depth);
+            glVertex3f(-0.05, -0.45, depth);
+        glEnd();
+        glBegin(GL_QUADS);
+            glVertex3f(-0.05, -0.5, depth);
+            glVertex3f(0.85, -0.5, depth);
+            glVertex3f(0.85, -0.7, depth);
+            glVertex3f(-0.05, -0.7, depth);
+        glEnd();
+
+        //TV
+        glColor3f(0.2f, 0.2f, 0.2f);
+        glBegin(GL_QUADS);//Base
+            glVertex3f(0.1, -0.2, depth);
+            glVertex3f(0.1, -0.125, depth);
+            glVertex3f(0.7, -0.125, depth);
+            glVertex3f(0.7, -0.2, depth);
+        glEnd();
+        glBegin(GL_QUADS);//Connection
+            glVertex3f(0.35, -0.125, depth);
+            glVertex3f(0.35, -0.075, depth);
+            glVertex3f(0.45, -0.075, depth);
+            glVertex3f(0.45, -0.125, depth);
+        glEnd();
+        glBegin(GL_QUADS);//Screen Container
+            glVertex3f(0.0, -0.075, depth);
+            glVertex3f(0.0, 0.4, depth);
+            glVertex3f(0.8, 0.4, depth);
+            glVertex3f(0.8, -0.075, depth);
+        glEnd();
+        glColor3f(0.0f, 0.0f, 0.0f);
+        glBegin(GL_QUADS);//Screen
+            glVertex3f(0.05, -0.025, depth);
+            glVertex3f(0.05, 0.35, depth);
+            glVertex3f(0.75, 0.35, depth);
+            glVertex3f(0.75, -0.025, depth);
         glEnd();
     }
 };
